24690458e729b14f6d228c1ccb6b6ab19d8d9a72
draukadin <draukadin@gmail.com>
2017-12-28 02:09:51 -0800

    Implementing service to call leader board api

--- src/main/webapp/app/leader-board/leader-board.service.ts
+++ src/main/webapp/app/leader-board/leader-board.service.ts
@@ -1,8 +1,24 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {Http, Response} from '@angular/http';
+import {Observable} from 'rxjs/Rx';
+import {SERVER_API_URL} from '../app.constants';
+
+import { ResponseWrapper } from '../shared';
 
 @Injectable()
 export class RfbloyaltyLeaderBoardService {
 
-  constructor() { }
+    private resourceUrl = SERVER_API_URL + 'api/rfb-leaderBoard';
 
+    constructor(private http: Http) { }
+
+    query(locationId: number): Observable<ResponseWrapper> {
+        return this.http.get(`${this.resourceUrl}/${locationId}`)
+            .map((res: Response) => this.convertResponse(res));
+    }
+
+    private convertResponse(res: Response): ResponseWrapper {
+        const jsonResponse = res.json();
+        return new ResponseWrapper(res.headers, jsonResponse, res.status);
+    }
 }
